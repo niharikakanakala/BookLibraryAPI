fb8bd4546879cd481600e1e473dafe63c5c745e2
hackerearth444 <niharikakanakala@gmail.com>
2023-08-04 14:23:54 +0530

    migrations

--- BookLibrary.Tests/SampleIntegrationTests.cs
+++ BookLibrary.Tests/SampleIntegrationTests.cs
@@ -1,4 +1,7 @@
-using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.TestHost;
@@ -6,26 +9,23 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Xunit;
+using FluentAssertions;
 using System.Net;
 using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
-using ContactManagementSystem.WebAPI.Data;
-using ContactManagementSystem.WebAPI.Models;
-using ContactManagementSystem.WebAPI;
-using Xunit;
+using BookLibrary.WebAPI.Data;
+using BookLibrary.WebAPI.Models;
+using BookLibrary.WebAPI;
 
 namespace SampleIntegrationTests.Tests
 {
-    public class ContactManagementSystemIntegrationTests
+    public class BookLibraryIntegrationTests
     {
         private TestServer _server;
         private HttpClient _client;
 
-        public ContactManagementSystemIntegrationTests()
+        public BookLibraryIntegrationTests()
         {
             SetUpClient();
         }
@@ -36,12 +36,12 @@ namespace SampleIntegrationTests.Tests
                 .UseStartup<Startup>()
                 .ConfigureServices(services =>
                 {
-                    var context = new ContactContext(new DbContextOptionsBuilder<ContactContext>()
+                    var context = new BookContext(new DbContextOptionsBuilder<BookContext>()
                         .UseSqlite("DataSource=:memory:")
                         .EnableSensitiveDataLogging()
                         .Options);
 
-                    services.RemoveAll(typeof(ContactContext));
+                    services.RemoveAll(typeof(BookContext));
                     services.AddSingleton(context);
 
                     context.Database.OpenConnection();
@@ -61,79 +61,76 @@ namespace SampleIntegrationTests.Tests
         }
 
         [Fact]
-public async Task TestGetContactById()
-{
-    // Add test data to the database
-    var context = _server.Host.Services.GetRequiredService<ContactContext>();
-    var testContact = new Contact
-    {
-        Name = "Test Contact",
-        Email = "test@example.com",
-        PhoneNumber = "123-456-7890",
-        Address = "Test Address",
-        City = "Test City",
-        Country = "Test Country"
-    };
-    context.Contacts.Add(testContact);
-    context.SaveChanges();
+        public async Task TestGetBookById()
+        {
+            // Add test data to the database
+            var context = _server.Host.Services.GetRequiredService<BookContext>();
+            var testBook = new Book
+            {
+                Title = "Test Book",
+                Author = "Test Author",
+                Genre = "Test Genre",
+                PublicationYear = 2022,
+                IsAvailable = true
+            };
+            context.Books.Add(testBook);
+            context.SaveChanges();
 
-    // Perform the GET request to /api/contacts/{contactId}
-    var response = await _client.GetAsync($"/api/contacts/{testContact.Id}");
-    response.StatusCode.Should().Be(HttpStatusCode.OK);
+            // Perform the GET request to /api/books/{bookId}
+            var response = await _client.GetAsync($"/api/books/{testBook.Id}");
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
 
-    // Deserialize the response content and verify the result
-    var content = await response.Content.ReadAsStringAsync();
-    var contact = JsonConvert.DeserializeObject<Contact>(content);
-    contact.Should().NotBeNull();
-    contact.Name.Should().Be(testContact.Name);
-    contact.Email.Should().Be(testContact.Email);
-    contact.PhoneNumber.Should().Be(testContact.PhoneNumber);
-    contact.Address.Should().Be(testContact.Address);
-    contact.City.Should().Be(testContact.City);
-    contact.Country.Should().Be(testContact.Country);
-}
+            // Deserialize the response content and verify the result
+            var content = await response.Content.ReadAsStringAsync();
+            var book = JsonConvert.DeserializeObject<Book>(content);
+            book.Should().NotBeNull();
+            book.Title.Should().Be(testBook.Title);
+            book.Author.Should().Be(testBook.Author);
+            book.Genre.Should().Be(testBook.Genre);
+            book.PublicationYear.Should().Be(testBook.PublicationYear);
+            book.IsAvailable.Should().Be(testBook.IsAvailable);
+        }
 
-[Fact]
-public async Task TestDeleteAllContacts()
-{
-    // Add test data to the database
-    var context = _server.Host.Services.GetRequiredService<ContactContext>();
-    context.Contacts.Add(new Contact
-    {
-        Name = "Test Contact 1",
-        Email = "test1@example.com",
-        PhoneNumber = "111-111-1111",
-        Address = "Test Address 1",
-        City = "Test City 1",
-        Country = "Test Country 1"
-    });
+        [Fact]
+        public async Task TestDeleteAllBooks()
+        {
+            // Add test data to the database
+            var context = _server.Host.Services.GetRequiredService<BookContext>();
+            context.Books.Add(new Book
+            {
+                Title = "Test Book 1",
+                Author = "Test Author 1",
+                Genre = "Test Genre 1",
+                PublicationYear = 2022,
+                IsAvailable = true
+            });
 
-    context.Contacts.Add(new Contact
-    {
-        Name = "Test Contact 2",
-        Email = "test2@example.com",
-        PhoneNumber = "222-222-2222",
-        Address = "Test Address 2",
-        City = "Test City 2",
-        Country = "Test Country 2"
-    });
+            context.Books.Add(new Book
+            {
+                Title = "Test Book 2",
+                Author = "Test Author 2",
+                Genre = "Test Genre 2",
+                PublicationYear = 2023,
+                IsAvailable = false
+            });
 
-    context.SaveChanges();
+            context.SaveChanges();
 
-    // Perform the DELETE request to /api/contacts to delete all contacts
-    var deleteResponse = await _client.DeleteAsync("/api/contacts");
-    deleteResponse.StatusCode.Should().Be(HttpStatusCode.NoContent);
+            // Perform the DELETE request to /api/books to delete all books
+            var deleteResponse = await _client.DeleteAsync("/api/books");
+            deleteResponse.StatusCode.Should().Be(HttpStatusCode.NoContent);
 
-    // Verify that all contacts were deleted
-    var getAllResponse = await _client.GetAsync("/api/contacts");
-    getAllResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+            // Verify that all books were deleted
+            var getAllResponse = await _client.GetAsync("/api/books");
+            getAllResponse.StatusCode.Should().Be(HttpStatusCode.OK);
 
-    var content = await getAllResponse.Content.ReadAsStringAsync();
-    var contacts = JsonConvert.DeserializeObject<List<Contact>>(content);
-    contacts.Should().NotBeNull();
-    contacts.Count.Should().Be(0);
-}
+            var content = await getAllResponse.Content.ReadAsStringAsync();
+            var books = JsonConvert.DeserializeObject<List<Book>>(content);
+            books.Should().NotBeNull();
+            books.Count.Should().Be(0);
+        }
 
-
+        // Add more test methods for other book-related endpoints
+        // For example, TestAddBook, TestUpdateBook, TestGetAllBooks, etc.
     }
 }
---
+++ BookLibrary.WebAPI/Migrations/20230804085136_Migrations.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookLibrary.WebAPI.Migrations
+{
+    public partial class Migrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Author = table.Column<string>(type: "TEXT", nullable: true),
+                    Genre = table.Column<string>(type: "TEXT", nullable: true),
+                    PublicationYear = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsAvailable = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
